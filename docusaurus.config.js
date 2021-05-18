990abed05d767a554981f6e0c20d9a530ec20e0e
sosuisen <hidekazu.kubota@gmail.com>
2021-05-18 19:22:37 +0900

    fix: fix copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ module.exports = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Hidekazu Kubota.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Hidekazu Kubota`,
     },
   },
   presets: [
