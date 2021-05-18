5c377bb9a854e678d9a95cb3292ac348b3b296f0
sosuisen <hidekazu.kubota@gmail.com>
2021-05-18 19:19:35 +0900

    fix: fix path for editting on github.com

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,13 +97,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/sosuisen/gitddb-site/edit/master/website/',
+            'https://github.com/sosuisen/gitddb-site/edit/main/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/sosuisen/gitddb-site/edit/master/website/blog/',
+            'https://github.com/sosuisen/gitddb-site/edit/main/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
