f32e042331186aec0e50d4a7186e20a7a336bd27
sosuisen <hidekazu.kubota@gmail.com>
2021-08-25 15:20:17 +0900

    fix: bump to v0.4.4

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ module.exports = {
         {
           to: 'versions',
           position: 'right',
-          label: 'v0.4.3',
+          label: 'v0.4.4',
         },
         {
           href: 'https://github.com/sosuisen/git-documentdb',
